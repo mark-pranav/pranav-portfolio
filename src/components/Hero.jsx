edf287f6b038182156e7fccb25fb13718ae0fec8
Pranav <pranavbobade48@gmail.com>
2024-09-25 11:45:37 +0530

    K changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 
 import { HERO_CONTENT } from "../constants";
-import profilepic from "../assets/KevinRushProfile.jpg";
+import profilepic from "../assets/kevinRushProfile.jpg";
 import {motion} from "framer-motion";
 
 const container = (delay) => ({
